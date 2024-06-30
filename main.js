756857300bec9ca30bcb02c9c9890dff756d41ad
nat-Dcoder <nj433487@gmail.com>
2024-06-30 09:31:46 +0000

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(async () => {
     $('.control-menu').css('display', 'none')
-    $('.range-control').css('display', 'none');
+    $('.range-control').css('display', '');
     $('audio').removeAttr('controls');
     $('.controls-wrapper').css('visibility', 'visible');
     $('.fa-ellipsis-v').on('click', () => {
@@ -10,13 +10,37 @@ $(async () => {
     $('.fa.fa-heart').on('click', () => {
         $('.fa.fa-heart').css('color', 'red')
     })
-    $('.main-content').on('click', () => {
+
+    $(`
+        .main-content, 
+        .header, 
+        .fa-bell, 
+        .fa-heart,
+        .fa-volume,
+        button,
+        .main-controls,
+        .progress,
+        .spacer
+        
+    `).on('click', () => {
         $('.control-menu').hide();
     })
+
+    $('.v-icon').on('click', () => {
+        $('.range-container').toggle();
+    })
+
     
     //========== Volume slider ================
-    const tooltip = $('output[for="volume-slider"]');
+    const volumeIcon = $('.range-control i');
+    const volumeValue = Number($('#volume-slider').val());
 
+    const tooltip = $('output[for="volume-slider"]');
+    tooltip.text($('#volume-slider').val());  
+    volumeIcon.on('click', () => {
+        volumeIcon.toggleClass();
+        volumeIcon.addClass('fa fa-volume-xmark')
+    })
     $('#volume-slider').on('input', function () {
         const val = $(this).val();
         
@@ -24,55 +48,32 @@ $(async () => {
         // Calculate the position of the tooltip
         const trans = "-" + val + "%";
         const width = $(this).outerWidth();
-        tooltip.css({
-            left: val + "%",
-            transform: `translate(${trans})`
-        });
-    });
-    //========= Volume slider end ============
-
-    const client_id = '618d7377fa17420387ffc9ec9e12d7e4';
-    const client_secret = '1d30572400754b87a03a8816a827e767';
-    
-    // Fetch access token
-    const fetchSpotifyApiAccesssToken = async () => {
-        authOptions = {
-            method: 'POST',
-            url: 'https://accounts.spotify.com/api/token',
-            headers: {
-                'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: 'grant_type=client_credentials',
-            json: true
+        if (val > 60) {
+            volumeIcon.toggleClass();
+            volumeIcon.addClass('fa fa-volume-high')
+        }
+        else if (val > 1 || val < 35 ) {
+            volumeIcon.removeClass('fa fa-volume-high');
+            volumeIcon.addClass('fa fa-volume-down');
+        }
+        else if (val < 1) {
+            volumeIcon.removeClass('fa fa-volume-low');
+            volumeIcon.addClass('fa fa-volume-xmark')
         };
-        const res = await fetch(authOptions.url, authOptions);
-        const data = await res.json();
-        return data;
-    }
 
-    
-    const token = await fetchSpotifyApiAccesssToken();
-    const _getTracks = async () => {
-        const result = await fetch('https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl', {
-            method: 'GET',
-            headers: { Authorization:'Bearer ' + token.access_token,'Content-Type': 'application/json' }
-        });
-        const data = await result.json();
-        return data;
-    }
-
-    const  UIControl =  async () => {
-        const trackData = await _getTracks();
-        console.log(trackData)
-        $('audio').append(`<source src= ${trackData.external_urls.spotify} type="audio/mpeg">`);
+        $('v-icon').on('click', () => {
+            $('.range-control').slideToggle()
+        })
         
-        $(".fa-play").on('click', function() {
-        //$('audio')[0].play(); // Play audio
-        });
-    }
+    });
+
+    $('.fa.fa-volume-high').on('click', () => {
+        $('.range-control').show()
+    })
     
-    fetchSpotifyApiAccesssToken();
+    //========= Volume slider end ============
+    
+   
     _getTracks()
     UIControl()
 });
